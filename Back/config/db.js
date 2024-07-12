ea51dab0c29ef7530e3772edf54b303f2c60970e
Feliciano  MANDJAM <felicianomandja@gmail.com>
2024-07-12 11:05:07 +0200

    enl du dialiect sur db.js

--- Back/config/db.js
+++ Back/config/db.js
@@ -1,19 +1,14 @@
 import { Sequelize } from "sequelize";
 import { ENV } from "./env.js";
 
-const connection = new Sequelize(
-  ENV.DB_NAME,
-  "root",
-  "root",
-  {
-    host: ENV.DB_HOST,
-    dialect: 'mysql',
-    logging:false,
+const connection = new Sequelize(ENV.DB_NAME, "root", "root", {
+  host: ENV.DB_HOST,
+  dialect: "mysql",
+  logging: false,
 
-    dialectOptions: {
-      socketPath: "/Applications/MAMP/tmp/mysql/mysql.sock",
-    },
-  }
-);
+  // dialectOptions: {
+  //   socketPath: "/Applications/MAMP/tmp/mysql/mysql.sock",
+  // },
+});
 
 export default connection;
